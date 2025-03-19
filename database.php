2c34b5e52b82155f287117fa6ef73055fc08d6dc
shakibalam2503 <shakib.alam2503@gmail.com>
2025-03-20 01:11:57 +0600

    Add alert for successful connection

--- database.php
+++ database.php
@@ -1,14 +1,16 @@
 <?php
-$db_server="localhost";
-$db_user="root";
-$db_pass="";
-$db_name="catayon";
-$conn="";
-$conn=mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "catayon";
+
+// Establish connection
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+// Check connection
 if ($conn) {
-    echo "direct connection";
-}
-else{
-    echo "could not connect";
+    echo "<script>alert('Connection successful');</script>";
+} else {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
